a0936cdb4910ba5054f83120c3baae6f970f7ef4
Bruno Jullien <bruno.jullien@gmail.com>
2021-10-22 11:20:46 -0700

    35. Card Component

--- src/App.js
+++ src/App.js
@@ -25,11 +25,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <CardList name='Bruno'>
-          {this.state.monsters.map(monster => (
-            <h1 key={monster.id}> {monster.name} </h1>
-          ))}
-			  </CardList>
+        <CardList monsters={this.state.monsters}></CardList>
       </div>
     );
   }
